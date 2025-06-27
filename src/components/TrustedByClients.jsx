a0dc59afa16cffab3bb686ccdce6042d38e8e652
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-06-27 11:58:13 +0600

    Added Trusted by section

---
+++ src/components/TrustedByClients.jsx
@@ -0,0 +1,28 @@
+import amazonLogo1 from '../assets/brands/amazon.png';
+import casioLogo from '../assets/brands/casio.png';
+import moonstarLogo from '../assets/brands/moonstar.png';
+import randstadLogo from '../assets/brands/randstad.png';
+import startPeopleLogo1 from '../assets/brands/start-people 1.png';
+import startLogo2 from '../assets/brands/start.png';
+import amazonVector from '../assets/brands/amazon_vector.png';
+
+const TrustedByClients = () => {
+    return (
+        <div className="py-10 bg-white">
+            <h1 className="text-3xl font-bold text-center mb-6">Trusted By</h1>
+            <marquee behavior="scroll" direction="left" scrollamount="5">
+                <div className="flex gap-12 items-center">
+                    <img src={amazonLogo1} alt="Amazon" className="h-10 inline-block" />
+                    <img src={casioLogo} alt="Casio" className="h-10 inline-block" />
+                    <img src={moonstarLogo} alt="Moonstar" className="h-10 inline-block" />
+                    <img src={randstadLogo} alt="Randstad" className="h-10 inline-block" />
+                    <img src={startPeopleLogo1} alt="Start people" className="h-10 inline-block" />
+                    <img src={startLogo2} alt="Start people 2" className="h-10 inline-block" />
+                    <img src={amazonVector} alt="Amazon vector 2" className="h-10 inline-block" />
+                </div>
+            </marquee>
+        </div>
+    )
+}
+
+export default TrustedByClients;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../../components/Banner"
 import HowItWorks from "../../components/HowItWorks"
 import Services from "../../components/Services/Services"
+import TrustedByClients from "../../components/TrustedByClients"
 
 const Home = () => {
     return(
@@ -9,7 +10,7 @@ const Home = () => {
             <div className="w-12/13 mx-auto space-y-10 mb-10">
             <HowItWorks />
             <Services />
-            
+            <TrustedByClients />
             </div>
         </div>
     )
