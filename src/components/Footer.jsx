2abc2e69aa2e6dd375b4e34e1aab1685642b537e
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-06-27 07:21:19 +0600

    Footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,20 @@
 import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 import { Link } from "react-router";
+import profastLogo from '../assets/logo.png'
 
 const Footer = () => {
 
     return (
-        <footer className="footer footer-horizontal footer-center bg-primary text-primary-content p-10">
+        <footer className="footer footer-horizontal footer-center bg-neutral text-primary-content p-10">
             <aside>
+                <div className="relative">
+                    <img className="w-[80px] rounded-full" src={profastLogo} alt="" />
+                    <h2 className="text-2xl font-bold absolute top-11 left-14">ProFast</h2>
+                </div>
                 <p className="font-bold">
-                    ProFast Ltd.
-                    <br />
                     Providing reliable logistics since 2025
                 </p>
-                <p>Copyright © { new Date().getFullYear() } - All right reserved</p>
+                <p> Copyright © {new Date().getFullYear()} - All right reserved</p>
             </aside>
             <nav>
                 <div className="grid grid-flow-col gap-4">
